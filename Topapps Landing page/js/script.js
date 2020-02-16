abdac548629c63117c04adf4078b05d80f59b970
donirufatov151220 <42518828+donirufatov151220@users.noreply.github.com>
2020-02-16 15:30:53 +0500

    Add index.html and toppaps folder with layout

---
+++ Topapps Landing page/js/script.js
@@ -0,0 +1,24 @@
+$(function(){
+	$('.awesome-slider').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		arrows: false,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 2000
+	});
+
+	$('.reviews-slider').slick({
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		arrows: false,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 2000
+	});
+
+
+
+});
