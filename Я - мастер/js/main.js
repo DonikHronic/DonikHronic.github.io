7dcb7cd28e1df7627aab2ecb2cafc00c4f53e519
donikrufatov <donirufatov@mail.ru>
2020-10-07 14:50:09 +0500

    add ya-master

---
+++ Я - мастер/js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+
+	let burger_btn = $('.header__burger-menu');
+	let close_btn = $('.header__menu-close');
+	let menu = $('.header__menu-navigation');
+	let to_top = $('.to-top');
+
+	burger_btn.on('click', function () {
+		menu.addClass('active');
+		$('body').css('overflow-y', 'hidden')
+	});
+
+	close_btn.on('click', function () {
+		menu.removeClass('active');
+		$('body').css('overflow-y', 'visible')
+	});
+
+	let delay = 0;
+
+	to_top.click(function () { // При клике по кнопке "Наверх" попадаем в эту функцию
+		/* Плавная прокрутка наверх */
+		$('body, html').animate({
+			scrollTop: 0
+		}, delay);
+	});
+
+	$('.breadcrumbs__item:last a').addClass('active');
+
+});
